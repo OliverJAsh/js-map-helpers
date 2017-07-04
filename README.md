c270aecaa8df4dfce893fbb81e334cfc81275e1c
Oliver Joseph Ash <oliverjash@gmail.com>
2017-07-04 11:49:50 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,18 @@
+# js-map-helpers
+
+JavaScript `Map` helpers, e.g. `reduce`, `map`, `concat`.
+
+## Installation
+
+TODO: Publish
+
+## Development
+
+```
+yarn
+npm run start
+```
+
+## To do
+
+- [ ] `Set`
---
+++ src/index.ts
@@ -0,0 +1,33 @@
+export const normalizeMapToEntriesArray = <Key, Value>(map: Map<Key, Value>) =>
+    Array.from(map.entries());
+
+export const reduceMap = <Key, Value, Acc>(
+    reducer: (acc: Acc, value: Value, key: Key) => Acc,
+    initial: Acc,
+) => (map: Map<Key, Value>): Acc =>
+    normalizeMapToEntriesArray(map).reduce(
+        (innerAcc, [key, value]) => reducer(innerAcc, value, key),
+        initial,
+    );
+
+export const mapMap = <Key, ValueA, ValueB>(fn: (value: ValueA, key: Key) => ValueB) => (
+    map: Map<Key, ValueA>,
+): Map<Key, ValueB> =>
+    reduceMap<Key, ValueA, Map<Key, ValueB>>(
+        (acc, value, key) => acc.set(key, fn(value, key)),
+        new Map<Key, ValueB>(),
+    )(map);
+
+export const appendMap = <Key, Value>(
+    map1: Map<Key, Value>,
+    map2: Map<Key, Value>,
+): Map<Key, Value> => new Map([...map1, ...map2]);
+
+export const concatMap = <Key, Value>(maps: Map<Key, Value>[]): Map<Key, Value> =>
+    maps.reduce(appendMap, new Map<Key, Value>());
+
+// Examples
+
+console.log(concatMap([new Map([['foo', 'bar']]), new Map([['baz', 'bar']])]));
+
+console.log(mapMap((age: number) => age + 10)(new Map([['bob', 10]])));
